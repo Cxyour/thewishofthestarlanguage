f85ea36adc8fb292c5a50b4e5f3c9d8556a4c096
dwbdwbdwb <18500093258@163.com>
2018-05-04 16:09:28 +0800

    ahahhahahaha

--- app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/famousteacher/fragment/FamousTeacherFragment.java
+++ app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/famousteacher/fragment/FamousTeacherFragment.java
@@ -1,7 +1,6 @@
 package com.example.lenovo.thewishofthestarlanguage.view.famousteacher.fragment;
 
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.lenovo.thewishofthestarlanguage.R;
 import com.example.lenovo.thewishofthestarlanguage.contact.IFamousTeacherContract;
@@ -20,10 +19,7 @@ public class FamousTeacherFragment extends BaseFragment implements IFamousTeache
 
     @Override
     protected void init(View view) {
-          /*  famous_recycle=getView().findViewById(R.id.famous_recycle);
-            famous_recycle.setLayoutManager(new LinearLayoutManager(getContext()));
-            famous_recycle.setPullRefreshEnabled(true);
-            famous_recycle.setLoadingMoreEnabled(true);*/
+
         FamousTeacherPresenter famousTeacherPresenter = new FamousTeacherPresenter(this);
         famousTeacherPresenter.loadFrmousBean();
     }
@@ -37,6 +33,7 @@ public class FamousTeacherFragment extends BaseFragment implements IFamousTeache
 
     @Override
     public void showFamousTecah(FamousTeacherBean famousTeacherBean) {
-            Toast.makeText(getContext(), famousTeacherBean.getMessage(), Toast.LENGTH_SHORT).show();
+        FamousTeacherBean.DataBean data = famousTeacherBean.getData();
+
     }
 }

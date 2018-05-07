29c0518d69a00d6b32c88a5d7ec10c62a2701b08
Cxyour <461900240@qq.com>
2018-05-07 14:59:45 +0800

    qqq

--- app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/preview/fragment/PreviewFragment.java
+++ app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/preview/fragment/PreviewFragment.java
@@ -1,6 +1,19 @@
 package com.example.lenovo.thewishofthestarlanguage.view.preview.fragment;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.ContextMenu;
+import android.view.Gravity;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import com.example.lenovo.thewishofthestarlanguage.R;
 import com.example.lenovo.thewishofthestarlanguage.contact.PreviewContract;
@@ -12,8 +25,14 @@ import com.wuxiaolong.pullloadmorerecyclerview.PullLoadMoreRecyclerView;
 
 import java.util.List;
 
-public class PreviewFragment extends BaseFragment implements PreviewContract.view{
+/**
+ *
+ */
+public class PreviewFragment extends BaseFragment implements PreviewContract.view {
     private PullLoadMoreRecyclerView preview_recycle;
+    private CheckBox preview_cb_time_screen;
+    private View inflate;
+    private PopupWindow popupWindow;
 
     @Override
     protected int getLayoutId() {
@@ -22,7 +41,25 @@ public class PreviewFragment extends BaseFragment implements PreviewContract.vie
 
     @Override
     protected void init(View view) {
-        preview_recycle=view.findViewById(R.id.preview_recycle);
+        preview_recycle = view.findViewById(R.id.preview_recycle);
+        preview_cb_time_screen = view.findViewById(R.id.preview_cb_time_screen);
+        inflate = getLayoutInflater().inflate(R.layout.popup_item, null);
+        preview_cb_time_screen.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    preview_cb_time_screen.setText("取消");
+                    popupWindow = new PopupWindow(inflate, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT, false);
+                    popupWindow.setOutsideTouchable(true);
+                    popupWindow.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
+                    popupWindow.showAsDropDown(preview_cb_time_screen, 0, 0);
+                } else {
+                    preview_cb_time_screen.setText("时间筛选");
+                    popupWindow.dismiss();
+                }
+
+            }
+        });
         preview_recycle.setLinearLayout();
 
     }
@@ -39,4 +76,5 @@ public class PreviewFragment extends BaseFragment implements PreviewContract.vie
         PreviewAdapter previewAdapter = new PreviewAdapter(list);
         preview_recycle.setAdapter(previewAdapter);
     }
+
 }

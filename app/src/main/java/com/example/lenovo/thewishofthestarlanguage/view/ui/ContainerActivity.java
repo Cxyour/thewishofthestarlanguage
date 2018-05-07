24170d976e993b17da1b4c14c989c362763be1e7
Cxyour <461900240@qq.com>
2018-05-07 16:02:34 +0800

    qqq

--- app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/ui/ContainerActivity.java
+++ app/src/main/java/com/example/lenovo/thewishofthestarlanguage/view/ui/ContainerActivity.java
@@ -1,6 +1,7 @@
 package com.example.lenovo.thewishofthestarlanguage.view.ui;
 
 import android.app.FragmentManager;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -36,6 +37,9 @@ public class ContainerActivity extends BaseActivity implements View.OnClickListe
 
     private LinearLayout home_bo;
     private FragmentManager fragmentManager;
+    private ImageView home_master_shape;
+    private ImageView home_publish_valuable;
+    private RelativeLayout home_title;
 
 
     @Override
@@ -45,33 +49,38 @@ public class ContainerActivity extends BaseActivity implements View.OnClickListe
 
     @Override
     protected void init() {
-        home_master_btn = (ImageView) findViewById(R.id.home_master_btn);
-        home_master_tv = (TextView) findViewById(R.id.home_master_tv);
-        home_master_tab = (RelativeLayout) findViewById(R.id.home_master_tab);
-        home_work_btn = (ImageView) findViewById(R.id.home_work_btn);
-        home_work_tv = (TextView) findViewById(R.id.home_work_tv);
-        home_work_tab = (RelativeLayout) findViewById(R.id.home_work_tab);
-        home_valuable_btn = (ImageView) findViewById(R.id.home_valuable_btn);
-        home_valuable_tv = (TextView) findViewById(R.id.home_valuable_tv);
-        home_valuable_tab = (RelativeLayout) findViewById(R.id.home_valuable_tab);
-        home_notice_btn = (ImageView) findViewById(R.id.home_notice_btn);
-        home_notice_tv = (TextView) findViewById(R.id.home_notice_tv);
-        home_notice_tab = (RelativeLayout) findViewById(R.id.home_notice_tab);
-        home_myself_btn = (ImageView) findViewById(R.id.home_myself_btn);
-        home_myself_tv = (TextView) findViewById(R.id.home_myself_tv);
-        home_myself_tab = (RelativeLayout) findViewById(R.id.home_myself_tab);
-        home_bottom_layout = (LinearLayout) findViewById(R.id.home_bottom_layout);
+        home_master_btn = findViewById(R.id.home_master_btn);
+        home_master_tv = findViewById(R.id.home_master_tv);
+        home_master_tab = findViewById(R.id.home_master_tab);
+        home_work_btn = findViewById(R.id.home_work_btn);
+        home_work_tv = findViewById(R.id.home_work_tv);
+        home_work_tab = findViewById(R.id.home_work_tab);
+        home_valuable_btn = findViewById(R.id.home_valuable_btn);
+        home_valuable_tv = findViewById(R.id.home_valuable_tv);
+        home_valuable_tab = findViewById(R.id.home_valuable_tab);
+        home_notice_btn = findViewById(R.id.home_notice_btn);
+        home_notice_tv = findViewById(R.id.home_notice_tv);
+        home_notice_tab = findViewById(R.id.home_notice_tab);
+        home_myself_btn = findViewById(R.id.home_myself_btn);
+        home_myself_tv = findViewById(R.id.home_myself_tv);
+        home_myself_tab = findViewById(R.id.home_myself_tab);
+        home_bottom_layout = findViewById(R.id.home_bottom_layout);
+        home_master_shape = findViewById(R.id.home_master_shape);
+        home_publish_valuable = findViewById(R.id.home_publish_valuable);
+        home_title = findViewById(R.id.home_title);
 
         home_master_tab.setOnClickListener(this);
         home_myself_tab.setOnClickListener(this);
         home_work_tab.setOnClickListener(this);
         home_valuable_tab.setOnClickListener(this);
         home_notice_tab.setOnClickListener(this);
+        home_master_shape.setOnClickListener(this);
+        home_publish_valuable.setOnClickListener(this);
     }
 
     @Override
     protected void loadData() {
-        setContentView(R.id.home_lay,FamousTeacherFragment.class,null);
+        setContentView(R.id.home_lay, FamousTeacherFragment.class, null);
     }
 
     /**
@@ -83,28 +92,37 @@ public class ContainerActivity extends BaseActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.home_master_tab:
-                setContentView(R.id.home_lay,HomeWorkFragment.class,null);
-                setContentView(R.id.home_lay,FamousTeacherFragment.class,null);
                 //名师
+                home_publish_valuable.setVisibility(View.GONE);
+                setContentView(R.id.home_lay, HomeWorkFragment.class, null);
+                setContentView(R.id.home_lay, FamousTeacherFragment.class, null);
                 break;
             case R.id.home_work_tab:
                 //作业
-                setContentView(R.id.home_lay,HomeWorkFragment.class,null);
+                home_publish_valuable.setVisibility(View.GONE);
+                setContentView(R.id.home_lay, HomeWorkFragment.class, null);
                 break;
             case R.id.home_valuable_tab:
                 //宝典
-                setContentView(R.id.home_lay,TreasureFragment.class,null);
+                setContentView(R.id.home_lay, TreasureFragment.class, null);
                 break;
             case R.id.home_notice_tab:
                 //预告
-                setContentView(R.id.home_lay,PreviewFragment.class,null);
+                home_publish_valuable.setVisibility(View.GONE);
+                setContentView(R.id.home_lay, PreviewFragment.class, null);
                 break;
             case R.id.home_myself_tab:
-                setContentView(R.id.home_lay,PersonalFragment.class,null);
                 //我的
+                home_title.setVisibility(View.GONE);
+                setContentView(R.id.home_lay, PersonalFragment.class, null);
+                break;
+
+            case R.id.home_master_shape:
+                break;
+
+            case R.id.home_publish_valuable:
                 break;
         }
     }
-
 
 }
